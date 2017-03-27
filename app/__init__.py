b304971d0d2f56c7f3188e76dc38dab031785bd1
Mary Clark <mmclark317@gmail.com>
2017-03-27 15:38:18 +0000

    Added in forms; committing so I can pull in Molly's template

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask import Flask
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
 
+# Uncomment this in production
+# application = app
+
 # Load the views
 from app import views, models, controllers
 
---
+++ app/views/__init__.py
@@ -0,0 +1,3 @@
+# Import template rendering for all of the views
+from flask import render_template
+from app import app
