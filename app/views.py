6f2ec3bfe8bc84b1f546fd445a9ddfa44a6eb8d7
Mary Clark <mmclark317@gmail.com>
2017-03-28 14:11:37 +0000

    Interface changes: - Template-ified the theme. - Removed some elements from the index that were unnecessary, but this needs more refinement later. - Forms now show up correctly with the templating engine, have their own form_base file. - Currently working on fleshing on the continuation form first.

--- app/views.py
+++ app/views.py
@@ -5,17 +5,16 @@ from app import app
 
 @app.route('/')
 def index():
-    return "Hello World!"
-    #render_template('index.html')
+    return render_template("index.html")
 
 @app.route('/forms/continuation')
 def continuationForm():
-    render_template("forms/continuation.html")
+    return render_template("forms/continuation.html")
 
 @app.route('/forms/internship')
 def internshipForm():
-    render_template("forms/internship.html")
+    return render_template("forms/internship.html")
 
 @app.route('/forms/admission')
 def admissionForm():
-    render_template("forms/admission.html")
+    return render_template("forms/admission.html")
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 # On Cloud9:
+# export FLASK_APP=run.py
 # flask run --host '0.0.0.0' --port '8080' 
 
 from app import app 
