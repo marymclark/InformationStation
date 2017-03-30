859d59476cd7996fa43a92f9b00f730190c8b8be
Mary Clark <mmclark317@gmail.com>
2017-03-30 14:48:47 +0000

    Committing and pushing my work for the morning. Dropdowns 1 and 2 for endorsement area mostly working. Will be refactored later.

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,6 +1,6 @@
 // Form Functions
 
-// TODO this needs to take asynchronous callback
+// TODO once this is worked out and runs okay, needs to be refactored
 function buildEndorsementArea() {
     // Import JSON object with endorsements
     $.getJSON("/api/endorsements", function(data) {
@@ -9,20 +9,48 @@ function buildEndorsementArea() {
         
         var first_level = [];
         for (var key in data) {
-            first_level.push([data[key].title, key]); // key seems redundant but I promise I'm trying something here
+            first_level.push(data[key].title);
         }
-        console.log(first_level);
+        console.log("first_level:"+first_level);
         
         // Create a dropdown from that first level
         $("#endorsementArea").append('<div class="col-sm-4"><select class="form-control" id="endorsementArea1"></select></div>');
         for (var key in first_level) {
-            $("#endorsementArea1").append('<option value="'+first_level[key][1]+'">'+first_level[key][0]+'</option>');
+            $("#endorsementArea1").append('<option value="'+key+'">'+first_level[key]+'</option>');
         }
         
         // Now listen for the value in the dropdown to change
-        
         // When something in the first level is chosen, 
         // update a second dropdown with the first level's subcategories
+        $("#endorsementArea1").change(function() {
+            var index = $("#endorsementArea1").val();
+            let selection = data[index]; 
+            if (selection.subcategories != undefined) { // If subcategories exist
+                // Build second_level
+                var second_level = [];
+                for (var key in selection.subcategories) {
+                    second_level.push(selection.subcategories[key].title);
+                }
+                console.log("second_level:"+second_level);
+                
+                // Create if doesn't already exist
+                if (!$('#endorsementArea2').length) { // If endorsementarea2 isn't already there
+                    $("#endorsementArea").append('<div class="col-sm-4"><select class="form-control" id="endorsementArea2"></select></div>');
+                }
+                
+                // Empty and add new elements
+                $("#endorsementArea2").empty();
+                for (var key in second_level) {
+                    $("#endorsementArea2").append('<option value="'+key+'">'+second_level[key]+'</option>');
+                }
+            } 
+            else {
+                console.log("No subcategories.");
+                // Clear
+                $("#endorsementArea2").empty();
+                // Hide the dropdown
+            }
+        });
         
         // When something in the second level is chosen,
         // update a third dropdown with the first level's subcategories
