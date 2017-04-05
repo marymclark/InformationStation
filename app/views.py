3959e4bec96b5d48a3be8e4670c484e64ee6189a
hzontine <hzontine@mail.umw.edu>
2017-04-05 15:35:14 +0000

    Login working, register almost

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 # Views to display all forms
 
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 from app import app
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import helpers
+#from flask_sqlalchemy import SQLAlchemy
+
+#db = SQLAlchemy(app)
+
+
 
 # Data for Javascript
 
@@ -25,21 +31,36 @@ def index():
     
     
 # Login
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    
-    
-    return "x"
-    
-    
+    form = helpers.LoginForm(request.form)
+    print form.errors
+    if request.method == 'POST':
+        if form.validate():
+            email=request.form['email']
+            password=request.form['password']
+            print email
+            print password
+#            cur = mysql.connection.cursor()
+#            cur.execute("""select * from users where email = %s AND password = crypt(%s, password);""", (email, password))
+#            results = cur.fetchone()
+#            print results
+            if ((email == "testingteam@umw.edu" and password == "coeas") or
+                (email == "hzontine@umw.edu" and password == "coeas")):
+                print "Logged In"
+                return render_template("userdash.html", user=email)
+            else:
+                print "No account associated with that email and password"
+        else:
+            print "Error-- missing email and/or password"
+    return render_template("login.html")
+
 # Logout
 @app.route('/logout')
 def logout():
+    return render_template("index.html")      
     
-    
-    return "x"        
-    
-    
+
     
 # User Dashboard
 @app.route('/dashboard')
