6e7da28242c4fa458070f60dcbc93d87a7fe2050
acarlyle <cyheddie@gmail.com>
2017-04-07 20:43:24 +0000

    Home, auth blueprints implemented.  Register/login code needs to go in templates/auth and app/auth/forms.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
 from flask_migrate import Migrate
-import helpers
+from flask_bootstrap import Bootstrap
 
+
+import helpers
 from config import app_config
 
 db = SQLAlchemy()
@@ -13,18 +15,18 @@ login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    #app.config.from_object(app_config[config_name])
-    #app.config.from_pyfile('config.py')
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    Bootstrap(app)
     db.init_app(app)
-    
     login_manager.init_app(app)
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
-    
     migrate = Migrate(app, db)
 
     from app import models
-    
+
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
@@ -33,6 +35,5 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
-
 
     return app
