a508d799cd2efb83534adfa5f727cdc16f52da44
acarlyle <cyheddie@gmail.com>
2017-04-07 01:38:37 +0000

    Broken: Added initial Models.py code for SQLAlchemy Database, need to add Migration subdirectory after error is fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 # File to initialize the application
 
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
+from flask_migrate import Migrate
 import helpers
 
 # Initialize the app
@@ -12,11 +13,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message = "Please log in to access this page."
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return helpers.User.get(user_id)
 
 # Uncomment this in production
 # application = app
@@ -29,5 +25,6 @@ app.config.from_object('config')
 
 app.secret_key = "not so secret"
 
-#db = SQLAlchemy(app)
+db = SQLAlchemy()
+migrate = Migrate(app, db)
 #db.init_app(app)
---
+++ app/models.py
@@ -0,0 +1,49 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login_manager
+
+class User(UserMixin, db.Model):
+    """
+    Create a User table
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    lastLoginDate = db.Column(db.DateTime, index=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.email)
+
+# Set up user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
