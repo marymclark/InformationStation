4e63eeca01837a30628568d4740a0526e2572ed8
acarlyle <cyheddie@gmail.com>
2017-04-17 14:58:36 +0000

    huge progress on reset password made, needs just a little bit more to tie together

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 # File to initialize the application
 
-from flask import Flask
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 
 import helpers
 from config import app_config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+mail= Mail()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -23,19 +25,21 @@ def create_app(config_name):
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
     migrate = Migrate(app, db)
+    mail = Mail(app)
+    mail.init_app(app)
 
     from app import models
+    with app.app_context():
+        from .admin import admin as admin_blueprint
+        app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+        from .auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
+        from .home import home as home_blueprint
+        app.register_blueprint(home_blueprint)
     
-    from .forms import forms as forms_blueprint
-    app.register_blueprint(forms_blueprint)
+        from .forms import forms as forms_blueprint
+        app.register_blueprint(forms_blueprint)
 
     return app
